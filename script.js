2b0105a16f58367b1d6f1fdd536588b51a351bf3
ashsass <93344091+ashsass@users.noreply.github.com>
2022-08-24 18:46:33 -0400

    Create the code for decrypt - copy and paste encrypt and modify it slightly

--- script.js
+++ script.js
@@ -36,10 +36,37 @@ class ShiftCipher{
     }
 
     decrypt(string){ 
+        let newString = ''; //Creates an empty string to return the encrypted version
+        for(let i = 0; i < string.length; i++){
+            let utfShifted = 0;
+            let utfNumber = string.toLowerCase().charCodeAt(i); //Loops through each place in the string, lower cases it, then gets the UTF code for the lowercase letter
+            if(utfNumber === 32 || utfNumber < 97 || utfNumber > 122){
+                newString += String.fromCharCode(utfNumber);
+                continue;
+            }//Skip the code that comes up for spaces or special characters
+            
+
+            utfShifted = utfNumber - this._numberShift; //Creating the shifted utfCode
+
+            if(utfShifted < 97){
+                utfShifted += this._numberShift;//Need to reset the utfShifted so that it will loop around to the end of the alphabet
+                for(let j = 1; j <= this._numberShift; j++){
+                    utfShifted -= 1;
+                    if(utfShifted === 96) //Check if the utf Code has gone before A
+                    {
+                        utfShifted = 122;
+                    }
+                }  
+            }
+            newString += String.fromCharCode(utfShifted);
+        }
+        console.log(newString);
         
     }
 }
     
 
 const cipher = new ShiftCipher(2);
-cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+// cipher.encrypt('I love to code!'); // returns 'K NQXG VQ EQFG!'
+cipher.decrypt('K <3 OA RWRRA'); // returns 'i <3 my puppy'
+
